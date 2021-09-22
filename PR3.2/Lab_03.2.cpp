fc9ed95eb267cb2c79b1128be4b712f10c2b77fb
unknown <deadmarvald@gmail.com>
2021-09-22 21:58:53 +0300

    Лабораторна робота №3.2

---
+++ PR3.2/Lab_03.2.cpp
@@ -0,0 +1,48 @@
+// Lab_03.2.cpp
+// Вольвенко Іван
+// Лабораторна робота №3.2
+// Розгалуження, задане формулою: функція з параметрами.
+// Варіант 13
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double a;
+	double b;
+	double c;
+	double F;
+
+	cout << "x = "; cin >> x;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	
+// спосіб 1: розгалуження в скороченій формі
+	if (x - 1 < 0 && b - x != 0)
+		F = a * x * x + b;
+	if (x-1 > 0 && b + x == 0)
+		F = (x - a) / x;
+	if (!(x - 1 < 0 && b - x != 0) && !(x - 1 > 0 && b + x == 0))
+		F = x / c;
+ 
+	cout << endl;
+	cout << "1) F = " << F << endl;
+
+// спосіб 2: розгалуження в повній формі
+	if (x - 1 < 0 && b - x != 0)
+		F = a * x * x + b;
+	else 
+		if (x - 1 > 0 && b + x == 0)
+			F = (x - a) / x;
+		else 
+			F = x / c;
+	cout << "2) F = " << F << endl;
+
+	cin.get();
+	return 0;
+}
